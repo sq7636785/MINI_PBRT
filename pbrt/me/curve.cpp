5d3c2a99e01368feadee3b094143ad0c5b464f14
s7 <799671203@qq.com>
2018-12-04 11:05:47 +0800

    12.4.11.05

--- pbrt/me/curve.cpp
+++ pbrt/me/curve.cpp
@@ -59,7 +59,27 @@ static Point3f EvalBezier(const Point3f cp[4], Float u, Vector3f* deriv = nullpt
 	return Lerp(u, cp2[0], cp2[1]);
 }
 
+static bool CheckBoundingBox(const Point3f* cp, const Float maxWidth, const Ray& ray) {
+	//check y axis
+	if (std::max(std::max(cp[0].y, cp[1].y), std::max(cp[2].y, cp[3].y)) + 0.5f * maxWidth < 0 ||
+		std::min(std::min(cp[0].y, cp[1].y), std::min(cp[2].y, cp[3].y)) - 0.5f * maxWidth > 0) {
+		return false;
+	}
 
+	//check x axis
+	if (std::max(std::max(cp[0].x, cp[1].x), std::max(cp[2].x, cp[3].x)) + 0.5f * maxWidth < 0 ||
+		std::min(std::min(cp[0].x, cp[1].x), std::min(cp[2].x, cp[3].x)) - 0.5f * maxWidth > 0) {
+		return false;
+	}
+
+	//check z axis
+	Float zMax = ray.tMax * ray.d.Length();
+	if (std::max(std::max(cp[0].z, cp[1].z), std::max(cp[2].z, cp[3].z)) + 0.5f * maxWidth < 0 ||
+		std::min(std::min(cp[0].z, cp[1].z), std::min(cp[2].z, cp[3].z)) - 0.5f * maxWidth > zMax) {
+		return false;
+	}
+	return false;
+}
 
 CurveCommon::CurveCommon(const Point3f c[4], Float w0, Float w1, CurveType type, const Normal3f* norm)
 	: type(type){
@@ -97,8 +117,198 @@ std::vector<std::shared_ptr<Shape>> CreateCurve(
 	return segments;
 }
 
+Bounds3f Curve::ObjectBound() const {
+	Point3f cpObj[4];
+	cpObj[0] = BlossomBezier(common->cpObj, uMin, uMin, uMin);
+	cpObj[1] = BlossomBezier(common->cpObj, uMin, uMin, uMax);
+	cpObj[2] = BlossomBezier(common->cpObj, uMin, uMax, uMax);
+	cpObj[3] = BlossomBezier(common->cpObj, uMax, uMax, uMax);
+	Bounds3f b = Union(Bounds3f(cpObj[0], cpObj[1]), Bounds3f(cpObj[2], cpObj[3]));
+	Float width[2] = {
+		Lerp(uMin, common->width[0], common->width[1]),
+		Lerp(uMax, common->width[0], common->width[1])
+	};
+	return Expand(b, std::max(width[0], width[1]) * 0.5f);
+}
+
+Float Curve::Area() const {
+	Point3f cpObj[4];
+	cpObj[0] = BlossomBezier(common->cpObj, uMin, uMin, uMin);
+	cpObj[1] = BlossomBezier(common->cpObj, uMin, uMin, uMax);
+	cpObj[2] = BlossomBezier(common->cpObj, uMin, uMax, uMax);
+	cpObj[3] = BlossomBezier(common->cpObj, uMax, uMax, uMax);
+	Float width0 = Lerp(uMin, common->width[0], common->width[1]);
+	Float width1 = Lerp(uMax, common->width[0], common->width[1]);
+	Float avgWidth = (width0 + width1) * 0.5f;
+	Float approxLength = 0.f;
+	for (int i = 0; i < 3; ++i) {
+		approxLength += Distance(cpObj[i], cpObj[i + 1]);
+	}
+	return approxLength * avgWidth;
+}
 
 
+bool Curve::Intersect(const Ray& r, Float* tHit, SurfaceInteraction* isect, bool testAlphaTexture) const {
+	ProfilePhase p(isect ? Prof::CurveIntersect : Prof::CurveIntersectP);
+	++nTests;
+	Vector3f oErr, dErr;
+	Ray ray = (*WorldToObject)(r, &oErr, &dErr);
+
+	// Compute object-space control points for curve segment, _cpObj_
+	Point3f cpObj[4];
+	cpObj[0] = BlossomBezier(common->cpObj, uMin, uMin, uMin);
+	cpObj[1] = BlossomBezier(common->cpObj, uMin, uMin, uMax);
+	cpObj[2] = BlossomBezier(common->cpObj, uMin, uMax, uMax);
+	cpObj[3] = BlossomBezier(common->cpObj, uMax, uMax, uMax);
+
+	//ray coordinate system
+	Vector3f dx = Cross(ray.d, cpObj[3] - cpObj[0]);
+	if (dx.LengthSquared() == 0) {
+		Vector3f dy;
+		CoordinateSystem(ray.d, &dx, &dy);
+	}
+
+	//transform control point to ray coordinate system
+	Transform objectToRay = LookAt(ray.o, ray.o + ray.d, dx);
+	Point3f cp[4] = {
+		objectToRay(cpObj[0]),
+		objectToRay(cpObj[1]),
+		objectToRay(cpObj[2]),
+		objectToRay(cpObj[3])
+	};
+
+	//check bounding box intersect
+	//in ray coordinate system, ray.o in (0, 0, 0), ray.d in (0, 0, 1),
+	//so ,if the x y axis max value < 0 || min value > 0, the intesect false
+	Float maxWidth = std::max(Lerp(uMin, common->width[0], common->width[1]),
+		                      Lerp(uMax, common->width[0], common->width[1]));
+	if(!CheckBoundingBox(cp, maxWidth, ray)) { return false; }
+
+	//calculate recursive depth, not understand
+	Float L0 = 0;
+	for (int i = 0; i < 2; ++i)
+		L0 = std::max(
+			L0, std::max(
+				std::max(std::abs(cp[i].x - 2 * cp[i + 1].x + cp[i + 2].x),
+					std::abs(cp[i].y - 2 * cp[i + 1].y + cp[i + 2].y)),
+				std::abs(cp[i].z - 2 * cp[i + 1].z + cp[i + 2].z)));
+
+	Float eps =
+		std::max(common->width[0], common->width[1]) * .05f;  // width / 20
+	auto Log2 = [](Float v) -> int {
+		if (v < 1) return 0;
+		uint32_t bits = FloatToBits(v);
+		// https://graphics.stanford.edu/~seander/bithacks.html#IntegerLog
+		// (With an additional add so get round-to-nearest rather than
+		// round down.)
+		return (bits >> 23) - 127 + (bits & (1 << 22) ? 1 : 0);
+	};
+	// Compute log base 4 by dividing log2 in half.
+	int r0 = Log2(1.41421356237f * 6.f * L0 / (8.f * eps)) / 2;
+	int maxDepth = Clamp(r0, 0, 10);
+	ReportValue(refinementLevel, maxDepth);
+
+	return recursiveIntersect(ray, tHit, isect, cp, Inverse(objectToRay), uMin, uMax, maxDepth);
+
+}
+
+
+bool Curve::recursiveIntersect(const Ray& ray, Float* tHit, SurfaceInteraction* isect, 
+	                           const Point3f cp[4], const Transform& rayToObject, 
+	                           Float u0, Float u1, int depth) const {
+	Float rayLength = ray.d.Length();
+	if (depth > 0) {
+		//segment intersect
+		Point3f cpSplit[7];
+		SubdivideBezier(cp, cpSplit);
+
+		bool hit = false;
+		Float u[3] = { u0, (u0 + u1) / 2, u1 };
+
+		const Point3f* cps = cpSplit;
+		for (int seg = 0; seg < 2; ++seg, cps += 3) {
+			Float maxWidth = std::max(Lerp(u[seg], common->width[0], common->width[1]),
+				                      Lerp(u[seg + 1], common->width[0], common->width[1]));
+
+			//check bound box
+			if (!CheckBoundingBox(cps, maxWidth, ray)) { continue; }
+			
+			hit |= recursiveIntersect(ray, tHit, isect, cps, rayToObject, u[seg], u[seg + 1], depth - 1);
+
+			if (hit && ! tHit) return true;
+		}
+
+	} else {
+		// Intersect ray with curve segment
+		//first test xy(0,0) if inside the control point boundry
+		//it should in start edge right and end edge left, or it doesn't have intersect 
+		//start point
+		//edge function, it onlyt need start and end control point and a point in their normal direction
+		Float edge = (cp[1].y - cp[0].y) * -cp[0].y + cp[0].x * (cp[0].x - cp[1].x);
+		if (edge < 0) { return false; }
+		//end point
+		edge = (cp[2].y - cp[3].y) * -cp[3].y + cp[3].x * (cp[3].x - cp[2].x);
+		if (edge < 0) { return false; }
+
+		// Compute line $w$ that gives minimum distance to sample point
+		//approximate the curve by the line(end point and start point)
+		Vector2f segmentDirection = Point2f(cp[3]) - Point2f(cp[0]);
+		Float denom = segmentDirection.LengthSquared();
+		if (denom == 0.f) { return false; }
+		
+		Float w = Dot(-Vector2f(cp[0]), segmentDirection) / denom;
+
+		// Compute $u$ coordinate of curve intersection point and 
+		Float u = Clamp(Lerp(w, u0, u1), u0, u1);
+		Float hitWidth = Lerp(u, common->width[0], common->width[1]);
+
+		Normal3f nHit;
+		Vector3f dpcdw;
+		//here the control point why not u? because the u is in uMin and uMax for origin cp
+		//here the cp may be the segments
+		Point3f pc = EvalBezier(cp, Clamp(w, 0, 1), &dpcdw);
+		//如果有交点，那么交点一定是距离z轴最近的一个点， 那么它的x，y坐标一定是距离原点最近的，所以可以以直代曲，
+		//投射到xy平面，求一个原点到直线的最短距离， 再判断最短距离和宽度的关系
+		Float ptCurveDist2 = pc.x * pc.x + pc.y * pc.y;
+		if (ptCurveDist2 > hitWidth * hitWidth * 0.25f) { return false; }
+		Float zMax = rayLength * ray.tMax;
+		if (pc.z < 0 || pc.z > zMax) { return false; }
+
+		//compute v 
+		Float ptCurveDist = std::sqrt(ptCurveDist2);
+		Float edgeFunc = dpcdw.x * -pc.y + pc.x * dpcdw.y;
+		Float v = (edgeFunc > 0) ? 
+			       0.5f + ptCurveDist / hitWidth : 
+			       0.5f - ptCurveDist / hitWidth;
+
+		if (tHit != nullptr) {
+			*tHit = pc.z / rayLength;
+			Vector3f pError(2 * hitWidth, 2 * hitWidth, 2 * hitWidth);
+
+			Vector3f dpdu, dpdv;
+			EvalBezier(common->cpObj, u, &dpdu);
+			CHECK_NE(Vector3f(0, 0, 0), dpdu) << "u = " << u << ", cp = " <<
+				common->cpObj[0] << ", " << common->cpObj[1] << ", " <<
+				common->cpObj[2] << ", " << common->cpObj[3];
+			if (common->type == CurveType::Ribbon) {
+
+			} else {
+				Vector3f dpduPlane = (Inverse(rayToObject))(dpdu);
+				Vector3f dpdvPlane = Normalize(Vector3f(-dpduPlane.y, dpduPlane.x, 0)) * hitWidth;
+				if (common->type == CurveType::Cylinder) {
+					Float theta = Lerp(v, -90.f, 90.f);
+					Transform rot = Rotate(-theta, dpduPlane);
+					dpdvPlane = rot(dpdvPlane);
+				}
+				dpdv = rayToObject(dpdvPlane);
+			}
+			*isect = (*ObjectToWorld)(SurfaceInteraction(ray(pc.z), pError, Point2f(u, v), -ray.d, dpdu, dpdv,
+				                      Normal3f(0, 0, 0), Normal3f(0, 0, 0), ray.time, this));
+		}
+		++nHits;
+		return true;
+	}
+}
 
 
 Interaction Curve::Sample(const Point2f &u, Float *pdf) const {
---
+++ pbrt/me/hair.h
@@ -0,0 +1,139 @@
+
+#if defined(_MSC_VER)
+#define NOMINMAX
+#pragma once
+#endif
+
+#ifndef PBRT_MATERIALS_HAIR_H
+#define PBRT_MATERIALS_HAIR_H
+
+// materials/hair.h*
+#include "material.h"
+#include "pbrt.h"
+#include "reflection.h"
+#include <array>
+
+namespace pbrt {
+
+// HairMaterial Declarations
+	class HairMaterial : public Material {
+	  public:
+		  HairMaterial(const std::shared_ptr <Texture<Spectrum>>& sigma_a,
+					   const std::shared_ptr <Texture<Spectrum>>& color,
+					   const std::shared_ptr <Texture<Float>>& eumelanin,
+					   const std::shared_ptr <Texture<Float>>& pheomelanin,
+					   const std::shared_ptr <Texture<Float>>& eta,
+					   const std::shared_ptr <Texture<Float>>& beta_m,
+					   const std::shared_ptr <Texture<Float>>& beta_n,
+					   const std::shared_ptr <Texture<Float>>& alpha) 
+		  : sigma_a(sigma_a),
+		    color(color),
+		    eumelanin(eumelanin),
+		    pheomelanin(pheomelanin),
+		    eta(eta),
+		    beta_m(beta_m),
+		    beta_n(beta_n),
+		    alpha(alpha) { }
+
+		  void ComputeScatteringFunctions(SurfaceInteraction *si, MemoryArena &arena, TransportMode mode, bool allowMultipleLobes) const;
+	  private:
+		std::shared_ptr<Texture<Spectrum>> sigma_a, color;
+		std::shared_ptr<Texture<Float>> eumelanin, pheomelanin;
+		std::shared_ptr<Texture<Float>> eta, beta_m, beta_n, alpha;
+
+};
+
+
+	HairMaterial *CreateHairMaterial(const TextureParams &mp);
+
+	// HairBSDF Constants
+	static const int pMax = 3;
+	static const Float SqrtPiOver8 = 0.626657069f;
+
+	// HairBSDF Declarations
+	class HairBSDF : public BxDF {
+	  public:
+		  HairBSDF(Float h, Float eta, const Spectrum& sigma_a, Float beta_m, Float beta_n, Float alpha);
+		  Spectrum f(const Vector3f &wo, const Vector3f &wi) const;
+		  Spectrum Sample_f(const Vector3f &wo, Vector3f *wi, const Point2f &sample, Float *pdf, BxDFType *sampledType /* = nullptr */) const;
+		  Float Pdf(const Vector3f &wo, const Vector3f &wi) const;
+		  std::string ToString() const;
+		  static Spectrum SigmaAFromConcentration(Float ce, Float cp);
+		  static Spectrum SigmaAFromReflectance(const Spectrum& c, Float beta_n);
+
+	  private:
+		  std::array<Float, pMax + 1> ComputeApPdf(Float cosThetaO) const;
+
+
+	  private:
+		const Float h;
+		const Float gammaO;
+		const Float eta;
+		const Spectrum sigma_a;
+		const Float beta_m;
+		const Float beta_n;
+		Float v[pMax + 1];
+		Float s;
+		Float sin2kAlpha[3];
+		Float cos2kAlpha[3];
+	};
+
+	// General Utility Functions
+	inline Float Sqr(Float v) { return v * v; }
+
+	template<int n>
+	static Float Pow(Float v) {
+		static_assert(n > 0, "Power can not be negative");
+		Float n2 = Pow<n / 2>(v);
+		return n2 * n2 * Pow<n & 1>(v);
+	}
+
+	template<>
+	static Float Pow<1>(Float v) {
+		return v;
+	}
+
+	template<>
+	static Float Pow<0>(Float v) {
+		 return 1;
+	}
+
+
+	inline Float SafeASin(Float x) {
+		CHECK(x >= -1.0001 && x <= 1.0001);
+		return std::asin(Clamp(x, -1, 1));
+	}
+
+	inline Float SafeSqrt(Float x) {
+		CHECK_GE(x, -1e-4);
+		return std::sqrt(std::max(Float(0), x));
+	}
+
+	// https://fgiesen.wordpress.com/2009/12/13/decoding-morton-codes/
+	static uint32_t Compact1By1(uint32_t x) {
+		// TODO: as of Haswell, the PEXT instruction could do all this in a
+		// single instruction.
+		// x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+		x &= 0x55555555;
+		// x = --fe --dc --ba --98 --76 --54 --32 --10
+		x = (x ^ (x >> 1)) & 0x33333333;
+		// x = ---- fedc ---- ba98 ---- 7654 ---- 3210
+		x = (x ^ (x >> 2)) & 0x0f0f0f0f;
+		// x = ---- ---- fedc ba98 ---- ---- 7654 3210
+		x = (x ^ (x >> 4)) & 0x00ff00ff;
+		// x = ---- ---- ---- ---- fedc ba98 7654 3210
+		x = (x ^ (x >> 8)) & 0x0000ffff;
+		return x;
+	}
+
+	static Point2f DemuxFloat(Float f) {
+		CHECK(f >= 0 && f < 1);
+		uint64_t v = f * (1ull << 32);
+		CHECK_LT(v, 0x100000000);
+		uint32_t bits[2] = {Compact1By1(v), Compact1By1(v >> 1)};
+		return {bits[0] / Float(1 << 16), bits[1] / Float(1 << 16)};
+	}
+
+}  // namespace pbrt
+
+#endif  // PBRT_MATERIALS_HAIR_H
