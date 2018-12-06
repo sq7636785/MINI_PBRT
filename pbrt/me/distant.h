952710a57bdcb75c3e30f7b11f4f97edead37bc7
s7 <799671203@qq.com>
2018-12-06 13:31:25 +0800

    12.6.13.31

--- pbrt/me/distant.h
+++ pbrt/me/distant.h
@@ -59,11 +59,11 @@ class DistantLight : public Light {
                        Float *pdf, VisibilityTester *vis) const;
     Spectrum Power() const;
     Float Pdf_Li(const Interaction &, const Vector3f &) const;
-    Spectrum Sample_Le(const Point2f &u1, const Point2f &u2, Float time,
-                       Ray *ray, Normal3f *nLight, Float *pdfPos,
-                       Float *pdfDir) const;
-    void Pdf_Le(const Ray &, const Normal3f &, Float *pdfPos,
-                Float *pdfDir) const;
+	Spectrum Sample_Le(const Point2f &u1, const Point2f &u2, Float time,
+		Ray *ray, Normal3f *nLight, Float *pdfPos,
+		Float *pdfDir) const;
+	void Pdf_Le(const Ray &, const Normal3f &, Float *pdfPos,
+		Float *pdfDir) const;
 
   private:
     // DistantLight Private Data
